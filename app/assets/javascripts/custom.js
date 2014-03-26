ac3b67c94ccb4cf9af9816728ee141eee519d983
Jason King <jason.king@covenanteyes.com>
2014-03-26 12:23:34 -0400

    Cancel existing request for search

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -1,9 +1,13 @@
+var request = null;
+var timeout = null;
 $(document).ready(function(){
 	$('#family_search').keydown(function(){
+		timeout = window.clearTimeout(timeout);
 		setTimeout(function() {
-			$.get( "/families/search/"+$('#family_search').val(), function( data ) {
+			if (request != null) request.abort();
+			request = $.get( "/families/search/"+$('#family_search').val(), function( data ) {
 				$( ".results" ).html( data );
 			});
-		}, 50);
+		}, 500);
 	});
 });
